5f58d243e29574baacfc6b82f42d4e6a26284413
zanic <thecrnac@gmail.com>
2017-10-25 15:00:54 +0200

    Minor

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, re, logging, time
+import sys, re, logging, time, subprocess
 from datetime import datetime
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
