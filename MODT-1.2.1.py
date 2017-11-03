e34d5abd8ce01f319600bed6e72ac1c046705d18
zanic <thecrnac@gmail.com>
2017-11-03 14:18:35 +0100

    Minor

--- MODT-1.2.1.py
+++ MODT-1.2.1.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python3
 
-import os, sys, time, subprocess, shlex, select, logging, fileinput
-import RPi.GPIO as GPIO
+import os, sys, time, subprocess, shlex, logging
 from datetime import datetime
-import paho.mqtt.client as mqtt
+from collection import OrderedDict
 
-
-#ORIGINALS
-log_file = "/home/pi/test.log"
-appdef = "/home/pi/SmartSense/appdef"
-net_faces = "/etc/network/interfaces"
-#BACKUPS
-appdef_backup = "/home/pi/SmartSense/appdef_backup"
-net_faces_backup = "/etc/network/interfaces_backup"
 
 logging.basicConfig(filename=log_file, level=logging.DEBUG)
+log = logging.getLogger('Tester')
 
 mbmb_power_pin = 11
 mbmb_reset_pin  =13
 mbmb_hard_power_pin = 15
 
-global gps_coords
+coord_dict = OrderedDict()
 
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(mbmb_reset_pin, GPIO.OUT)
-GPIO.setup(mbmb_power_pin, GPIO.OUT)
-GPIO.setup(mbmb_hard_power_pin, GPIO.OUT)
+timestamp_begin = datetime.now()
+timestamp_end = datetime.now()
 
 def modem_power_off():
 	GPIO.output(mbmb_power_pin, False)
@@ -48,6 +36,7 @@ def modem_power_on():
 	time.sleep(0.18)
 	GPIO.output(mbmb_power_pin, True)
 	return
+
 def modem_reset():
 	GPIO.output(mbmb_reset_pin, True)
 	time.sleep(1)
@@ -55,21 +44,6 @@ def modem_reset():
 	time.sleep(0.1)
 	GPIO.output(mbmb_reset_pin, True)
 	return
-
-'''
-just run process and return stdout.
-This is blocking
-'''
-def run_shell_process(cmd):
-	try:
-		output = subprocess.check_output(
-			cmd, shell=True,stderr=subprocess.STDOUT
-			)
-	except Exception as err:
-		print("cmd %s FAILED %r" %(cmd, err))
-		return str(err)
-	return output.decode(encoding="utf-8", errors="ignore").rstrip()
-
 
 def on_connect(client, userdata, rc):
     mqtt_subscribe(client)
@@ -85,101 +59,51 @@ def mqtt_subscribe(client):
     return
 
 def process_mqtt_message(msg):
-	global gps_coords
-	msg = msg.payload.decode('utf8').split(',')
-	logging.debug(msg[0] + " " + msg[2])
-	print(str(msg[0] + " " + msg[2]))
-	if float(msg[0]) > 1 and float(msg[2]) > 1:
-		gps_coords = gps_coords + 1
-		if gps_coords > 3:
-			logging.debug("Test is over")
-			do_cleanup()
+	match = re.search("smartcity/data/0/GPS", msg.topic)
+	if match:
+		msg = (msg.payload.decode('utf-8').split(','))
+		process_mqtt_gps_data(msg)
 
+def process_mqtt_gps_data(msg):
+	if float(msg[0]) > 0.15 and float(msg[2]) > 0.15:
+		coord_dict[datetime.now()] = str(msg[0]) + ":" + str(msg[2])
+		if len(coord_dict) > 100:
+			end_test()
 
-def disable_nmag():
-	logging.debug("Disabling nmag in appdef")
-	for line in fileinput.input(appdef, inplace=True):
-		print(line.rstrip().replace('nmag:', '#nmag:'))
-	
-	logging.debug("nmag is disabled")
+def start_test():
+	log.info("Starting test")
+	test_run_state = True
+	timestamp_begin = datetime.now()
+	dict_run_times[test_run_state] = timestamp_begin
+	log.info("Test started with time: " + timestamp_begin.strftime('%d.%m.%Y %H:%M:%S'))
+	while test_run_state != False:
+		time.sleep(10)
+	return 
 
+def end_test():
+	log.info("Stoping test")
+	test_run_state = False
+	timestamp_end = datetime.now()
+	log.info("Test ended with time: " + timestamp_end.strftime('%d.%m.%Y %H:%M:%S'))
+	make_report()
 
-def check_reset_counter():
-	result = run_shell_process("sudo uboot_env -n reset_counter | grep  -oE  '[[:digit:]]'")
-	return result
-
-def check_is_nmag_enabled():
-	logging.debug("Parsing appdef to see if nmag is enabled")
-	
-	with open(appdef) as f:
-		for line in f.readlines():
-			if "#nmag" in line:
-				return False
-	return True
-
-def enable_wifi_auto():
-	logging.debug("Setting wifi auto mode")
-	
-	with open(net_faces, 'r+') as f:
-		for line in f.readlines():
-			if "iface eth0 inet dhcp" in line:
-				f.write("\n auto wlan0")
-
-def check_file_exists(file):
-	return os.path.exists(file)
-
-
-def do_cleanup():
-	logging.debug('GPS working at %s' % (datetime.now()))
-	quit()
+def make_report():
+	log.info("Making report")
+	with open(report_file, 'a') as f:
+		f.write(timestamp_begin.strftime('%d.%m.%Y %H:%M:%S')
+			+ " : " + timestamp_end.strftime('%d.%m.%Y %H:%M:%S') + 
+			" diff=" + str((timestamp_end-timestamp_begin).total_seconds()) +
+			" seconds" + "\n")
 
 if __name__ == "__main__":
-	run_shell_process("sudo mount -o remount rw /")
-	logging.debug("Starting gps location test")
 
-	gps_coords = 0
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(mbmb_reset_pin, GPIO.OUT)
+	GPIO.setup(mbmb_power_pin, GPIO.OUT)
+	GPIO.setup(mbmb_hard_power_pin, GPIO.OUT)
 
-	#if it is first time starting, make backups
-	if check_file_exists(appdef_backup):
-		pass
-	else:
-		logging.debug("Making backup of %s" % appdef)
-		run_shell_process("sudo cp %s %s" % (appdef, appdef_backup))
-
-	if check_file_exists(net_faces_backup):
-		pass
-	else:
-		logging.debug("Making backup of %s" % net_faces)
-		run_shell_process("sudo cp %s %s" % (net_faces, net_faces_backup))
-
-	#Mount partition as rw
-	logging.debug("Mounting partition as read/write")
-	run_shell_process("sudo mount -o remount rw /")
-	logging.debug("Done")
-	#check if nmag is disabled
-	result = check_is_nmag_enabled()
-	logging.debug("Done")
-	if  result:
-		disable_nmag()
-		enable_wifi_auto()
-		#run_shell_process("sudo reboot")
-	else:
-		# if nmag is disabled then this must be second iteration
-		#  return it to zero for simplicity
-		if check_reset_counter() == 4:
-			run_shell_process("sudo uboot_env -n reset_counter -v 0")
-
-	temp = 0
-	while temp <2:
-		modem_power_off()
-		logging.debug("Modem powered off")
-		modem_power_on()
-		logging.debug("Modem powered on")
-		modem_reset()
-		logging.debug("Modem reset")
-		temp = temp + 1
-		if temp == 0:
-			time.sleep(2*60)
+	logging.debug("Starting MODT-1.2.1")
 
 	# MQTT Parameters
 	broker_address = "127.0.0.1"
@@ -192,9 +116,4 @@ if __name__ == "__main__":
 	mqttc.connect(broker_address, port=broker_port, keepalive=60)
 
 	mqttc.loop_start()
-	
-	start_time = datetime.now()
-	logging.debug('Modem reset at %s' %  str(start_time))
-
-	while True:
-		time.sleep(50000)
+	start_test()
